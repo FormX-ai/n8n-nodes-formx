399eb60a649f51817b30a73dc9961a8d455b37f1
pkong-ds <pkong.work@gmail.com>
2024-04-25 16:46:09 +0800

    feat: Implement n8n credential flow

--- credentials/FormXApi.credentials.ts
+++ credentials/FormXApi.credentials.ts
@@ -1,5 +1,11 @@
-import { IAuthenticateGeneric, ICredentialType, INodeProperties } from 'n8n-workflow';
+import {
+	IAuthenticateGeneric,
+	ICredentialTestRequest,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
 
+// ref https://docs.n8n.io/integrations/creating-nodes/build/reference/credentials-files/
 export class FormXApi implements ICredentialType {
 	name = 'formXApi';
 	displayName = 'FormX API';
@@ -7,19 +13,61 @@ export class FormXApi implements ICredentialType {
 	properties: INodeProperties[] = [
 		// TODO: Add access token & extractor ID
 		{
-			displayName: 'API Key',
-			name: 'apiKey',
+			displayName: 'Access Token',
+			name: 'accessToken',
 			type: 'string',
 			typeOptions: { password: true },
 			default: '',
+			required: true,
+		},
+		{
+			displayName: 'Extractor ID',
+			name: 'extractorId',
+			// extractorId no need to obscure
+			// eslint-disable-next-line n8n-nodes-base/cred-class-field-unobscured-sensitive-input
+			type: 'string',
+			default: '',
+			required: true,
 		},
 	];
 	authenticate: IAuthenticateGeneric = {
 		type: 'generic',
 		properties: {
-			qs: {
-				api_key: '={{$credentials.apiKey}}',
+			headers: {
+				'X-WORKER-TOKEN': '={{$credentials.accessToken}}',
+				'X-WORKER-EXTRACTOR-ID': '={{$credentials.extractorId}}',
 			},
 		},
+	};
+	test: ICredentialTestRequest = {
+		// dummy request to async extraction api, verify above credentials work
+		request: {
+			method: 'POST',
+			baseURL: 'https://worker.formextractorai.com',
+			url: '/v2/extract',
+			headers: {
+				'X-WORKER-ASYNC': 'true',
+				'X-WORKER-IMAGE-URL': 'https://formextractorai.com/sample-invoice-1.d551279a.jpg',
+			},
+		},
+		// ref https://github.com/FormX-ai/form-extractor/blob/master/formx/worker/responses.py
+		rules: [
+			{
+				type: 'responseCode',
+				properties: {
+					value: 403,
+					message:
+						'Invalid Access Token, please copy it from https://formextractorai.com/team/tokens',
+				},
+			},
+			{
+				type: 'responseCode',
+				properties: {
+					value: 404,
+					message:
+						'Invalid Extract ID, please copy it from FormX Portal > Your Extractor > Settings',
+				},
+			},
+		],
 	};
 }
