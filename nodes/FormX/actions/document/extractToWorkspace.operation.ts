1db0162e7e71c2c09977d83e461a0e6336a081a1
Davis Tang <ngkaokis@gmail.com>
2024-05-28 20:24:12 +0800

    refactor: Consolidate document extraction api calls
    ref #2586

--- nodes/FormX/actions/document/extractToWorkspace.operation.ts
+++ nodes/FormX/actions/document/extractToWorkspace.operation.ts
@@ -3,12 +3,11 @@ import {
 	IDataObject,
 	IDisplayOptions,
 	IExecuteFunctions,
-	IHttpRequestOptions,
-	IN8nHttpFullResponse,
 	INodeExecutionData,
 	INodeProperties,
 } from 'n8n-workflow';
-import { config } from '../../../config';
+import { extractToWorkspace, getAsyncExtractionResult } from '../../../apis/client';
+import { ExtractToWorkspaceAPIv2RequestHeaderData } from '../../../apis/schemas/extractToWorkspace';
 import { retry } from '../../../utils/retry';
 import { updateDisplayOptions } from '../../../utils/updateDisplayOptions';
 import { commonProperties } from './commonProperties';
@@ -49,50 +48,20 @@ export async function execute(this: IExecuteFunctions, i: number): Promise<INode
 	});
 	const additionalFields = this.getNodeParameter('additionalFields', i, {}) as Record<string, any>;
 
-	const requestOptions: IHttpRequestOptions = {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-			'X-WORKER-WORKSPACE-ID': workspaceId,
-			'X-WORKER-ENCODING': 'raw',
-			'X-WORKER-IMAGE-URL': imageUrl,
-			'X-WORKER-PDF-DPI': '150',
-			'X-WORKER-WORKSPACE-FILE-NAME': additionalFields?.['fileName'],
-			'X-WORKER-PROCESSING-MODE': additionalFields?.['processingMode'] ?? 'per-page',
-			'X-WORKER-AUTO-ADJUST-IMAGE-SIZE': additionalFields?.['autoAdjustImageSize'] ?? true,
-			'X-WORKER-OCR-ENGINE': additionalFields?.['ocrEngine'] ?? '',
-		},
-		method: 'POST',
-		url: `${config.formxWorkerBaseUrl}/v2/workspace`,
-	};
-
-	const response = (await this.helpers.httpRequestWithAuthentication.call(
-		this,
-		'formXApi',
-		requestOptions,
-	)) as IDataObject;
+	const response = await extractToWorkspace.call(this, {
+		imageUrl: imageUrl,
+		workspaceId: workspaceId,
+		...additionalFields,
+	} as ExtractToWorkspaceAPIv2RequestHeaderData);
 
 	const jobId = response.job_id;
-	let getResultResponse: IN8nHttpFullResponse | null = null;
 
 	// Polling to get result until it's ready / timeout
 	const result = await retry(
 		async () => {
-			const requestOptions: IHttpRequestOptions = {
-				headers: {
-					Accept: 'application/json',
-				},
-				method: 'GET',
-				url: `${config.formxWorkerBaseUrl}/v2/extract/jobs/${jobId}`,
-				returnFullResponse: true,
-			};
-			getResultResponse = (await this.helpers.httpRequestWithAuthentication.call(
-				this,
-				'formXApi',
-				requestOptions,
-			)) as IN8nHttpFullResponse;
-			if (getResultResponse.statusCode !== 201) {
-				return getResultResponse.body;
+			const response = await getAsyncExtractionResult.call(this, jobId);
+			if (response.status === 'ok') {
+				return response;
 			} else {
 				throw Error('Extraction result not ready');
 			}
@@ -101,7 +70,7 @@ export async function execute(this: IExecuteFunctions, i: number): Promise<INode
 	);
 
 	const executionData = this.helpers.constructExecutionMetaData(
-		this.helpers.returnJsonArray(result as IDataObject[]),
+		this.helpers.returnJsonArray(result),
 		{ itemData: { item: i } },
 	);
 
