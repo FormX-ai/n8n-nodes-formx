96e29bdb32a934cb451ae333be075e55e30b854d
Davis Tang <ngkaokis@gmail.com>
2024-06-05 19:00:06 +0800

    feat: Add workspaceId to credential and test with verify endpoint
    ref #2607

--- credentials/FormXApi.credentials.ts
+++ credentials/FormXApi.credentials.ts
@@ -1,9 +1,12 @@
 import {
-	IAuthenticateGeneric,
+	ICredentialDataDecryptedObject,
 	ICredentialTestRequest,
 	ICredentialType,
+	IDataObject,
+	IHttpRequestOptions,
 	INodeProperties,
 } from 'n8n-workflow';
+import { config } from '../nodes/config';
 
 // ref https://docs.n8n.io/integrations/creating-nodes/build/reference/credentials-files/
 export class FormXApi implements ICredentialType {
@@ -11,7 +14,6 @@ export class FormXApi implements ICredentialType {
 	displayName = 'FormX API';
 	documentationUrl = 'https://github.com/FormX-ai/n8n-nodes-formx';
 	properties: INodeProperties[] = [
-		// TODO: Add access token & extractor ID
 		{
 			displayName: 'Access Token',
 			name: 'accessToken',
@@ -21,33 +23,78 @@ export class FormXApi implements ICredentialType {
 			required: true,
 		},
 		{
+			displayName: 'Workspace/Extractor ID',
+			name: 'workspace_or_extractor',
+			type: 'options',
+			noDataExpression: true,
+			options: [
+				{
+					name: 'Extractor ID',
+					value: 'extractorId',
+				},
+				{
+					name: 'Workspace ID',
+					value: 'workspaceId',
+				},
+			],
+			default: 'extractorId',
+		},
+		{
 			displayName: 'Extractor ID',
 			name: 'extractorId',
-			// extractorId no need to obscure
-			// eslint-disable-next-line n8n-nodes-base/cred-class-field-unobscured-sensitive-input
 			type: 'string',
 			default: '',
 			required: true,
-		},
-	];
-	authenticate: IAuthenticateGeneric = {
-		type: 'generic',
-		properties: {
-			headers: {
-				'X-WORKER-TOKEN': '={{$credentials.accessToken}}',
-				'X-WORKER-EXTRACTOR-ID': '={{$credentials.extractorId}}',
+			displayOptions: {
+				show: {
+					workspace_or_extractor: ['extractorId'],
+				},
 			},
 		},
+		{
+			displayName: 'Workspace ID',
+			name: 'workspaceId',
+			type: 'string',
+			default: '',
+			required: true,
+			displayOptions: {
+				show: {
+					workspace_or_extractor: ['workspaceId'],
+				},
+			},
+		},
+	];
+	authenticate = async (
+		credentials: ICredentialDataDecryptedObject,
+		requestOptions: IHttpRequestOptions,
+	): Promise<IHttpRequestOptions> => {
+		const header: IDataObject = {
+			'X-WORKER-TOKEN': credentials.accessToken,
+			'X-WORKER-EXTRACTOR-ID': credentials.extractorId || undefined,
+			'X-WORKER-WORKSPACE-ID': credentials.workspaceId || undefined,
+		};
+
+		return {
+			...requestOptions,
+			headers: {
+				...requestOptions.headers,
+				...header,
+			},
+		};
 	};
 	test: ICredentialTestRequest = {
 		// dummy request to async extraction api, verify above credentials work
 		request: {
 			method: 'POST',
-			baseURL: 'https://worker.formextractorai.com',
-			url: '/v2/extract',
+			baseURL: config.formxWorkerBaseUrl,
+			url: '/verify-access',
 			headers: {
-				'X-WORKER-ASYNC': 'true',
-				'X-WORKER-IMAGE-URL': 'https://formextractorai.com/sample-invoice-1.d551279a.jpg',
+				'X-WORKER-TOKEN': '={{$credentials.accessToken}}',
+			},
+			body: {
+				should_check_usage_reach_limit: true,
+				extractor_id: '={{$credentials.extractorId}}',
+				workspace_id: '={{$credentials.workspaceId}}',
 			},
 		},
 		// ref https://github.com/FormX-ai/form-extractor/blob/master/formx/worker/responses.py
