5e4ef2b138e4cefcc6cc1696bb9a937fa92092e3
Davis Tang <ngkaokis@gmail.com>
2024-06-06 17:19:36 +0800

    feat: Add default config for production environment

--- nodes/config.ts
+++ nodes/config.ts
@@ -11,8 +11,18 @@ function required(value: string | undefined): string {
 	return value;
 }
 
-export const config: Config = {
+const isDev = process.env.NODE_ENV === 'development';
+
+const devConfig: Config = {
 	formxApiBaseUrl: required(process.env.FORMX_API_BASE_URL),
 	formxWorkerBaseUrl: required(process.env.FORMX_WORKER_BASE_URL),
 	formxPortalBaseUrl: required(process.env.FORMX_PORTAL_BASE_URL),
 };
+
+const defaultConfig: Config = {
+	formxApiBaseUrl: 'https://api.formextractorai.com',
+	formxWorkerBaseUrl: 'https://worker.formextractorai.com',
+	formxPortalBaseUrl: 'https://formextractorai.com',
+};
+
+export const config = isDev ? devConfig : defaultConfig;
