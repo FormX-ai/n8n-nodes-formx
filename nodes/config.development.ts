ae9dc423a857a68397a5a47789633ec661e1c513
Davis Tang <ngkaokis@gmail.com>
2024-06-07 14:12:58 +0800

    feat: Separate config into development and production

---
+++ nodes/config.development.ts
@@ -0,0 +1,12 @@
+function required(value: string | undefined): string {
+	if (!value) {
+		throw new Error('Missing required config');
+	}
+	return value;
+}
+
+export const config: Config = {
+	formxApiBaseUrl: required(process.env.FORMX_API_BASE_URL),
+	formxWorkerBaseUrl: required(process.env.FORMX_WORKER_BASE_URL),
+	formxPortalBaseUrl: required(process.env.FORMX_PORTAL_BASE_URL),
+};
---
+++ nodes/config.type.ts
@@ -0,0 +1,5 @@
+interface Config {
+	formxApiBaseUrl: string;
+	formxWorkerBaseUrl: string;
+	formxPortalBaseUrl: string;
+}
